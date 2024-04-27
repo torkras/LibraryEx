9c05bd3e8dd98833eac5fb1350b23e01e05bdd6b
torkras <149871061+torkras@users.noreply.github.com>
2024-04-27 13:57:51 +0200

    Código práctica JAR

---
+++ src/main/java/com/developJavaApps/Main.java
@@ -0,0 +1,11 @@
+package com.developJavaApps;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Intro with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Los alumnos de este curso aprenden Programación en JAVA");
+    }
+}
